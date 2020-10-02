36f321fe6e7fc6e386c72ec93b2ce657711f83bf
new4761 <brid47@outlook.com>
2020-10-02 10:59:02 +0700

    Add before out to work
    Add test Data ref yaml

--- desktopapp/src/models/ConfigtxConfig.ts
+++ desktopapp/src/models/ConfigtxConfig.ts
@@ -1,38 +1,42 @@
 // Class for create configtx,yaml for NetworkConfig
 const yaml = require('js-yaml');
 import { YamlConfig } from "yaml-config";
-import {FileYamlBuilder} from "../module/FileYamlBuilder"
+import { FileYamlBuilder } from "../module/FileYamlBuilder"
 class ConfigtxConfig extends FileYamlBuilder implements YamlConfig {
     //**********************************
     // variable for export file 
-    fileName:string;
-    defaultOutputPath:string;
+    fileName: string;
+    defaultOutputPath: string;
 
     //****************************************
-	// self class variable
-   Organization =[testData];
-	let testData= {
-		"test":{
-			Name:"test",
-			SkipiAsForeign:false,
-			ID:"test",
-			MSPDir:"msp"
-		}
-	}
-    constructor () {
+    // self class variable
+    testData = {
+        Name:"sad",
+         SkipiAsForeign: false,
+        ID: "asad",
+        MSPDir: "msp"
+    }
+    Organization = [this.testData,this.testData];
+    constructor() {
         super();
         this.fileName = "configtx.yaml";
         this.defaultOutputPath = "bin"
     }
-    getUserInput(){}
-	createFile(){
-		let src = "\n # This file is Generate from YamlClass";
-		src += yaml.dump({'Organization':this.Organization})
-	this.saveFile(this.defaultOutputPath,src,this.fileName);
-	}
-    editFile(){}
-    updateNetworkConfig(){}
+    d1 = new ChannelCapabilities ();
+    getUserInput() { }
+    createFile() {
+        let src = "\n # This file is Generate from YamlClass";
+ //       src += yaml.safeDump({"sas":this.Organization})
+        src += yaml.safeDump({"Channle":this.d1})     
+          
+        src += yaml.safeDump({"Channle":this.d1})     
+ this.saveFile(this.defaultOutputPath, src, this.fileName);
+    }
+    editFile() { }
+    updateNetworkConfig() { }
 }
-// default data for yaml file 
-class Capabilities {
+// default data for yaml file
+class  ChannelCapabilities {
+    V2_0:boolean=true
 }
+export default new ConfigtxConfig();
