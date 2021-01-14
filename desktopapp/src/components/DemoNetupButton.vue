bb1c3764b53269516d65b2fa8b01b447e91c4699
pisichi <win.pisichi@gmail.com>
2021-01-14 15:19:40 +0700

    test Json config & test netup

---
+++ desktopapp/src/components/DemoNetupButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-m-5">
+    <Button label="netup" @click="netup()" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import fs from "fs";
+import Component from "vue-class-component";
+import OSProcess from "../module/OSProcess";
+
+@Component({
+  components: {},
+})
+export default class DemoNetupButton extends Vue {
+  netup() {
+    console.log("up");
+    let rawdata = fs.readFileSync("./tests/test.json");
+    let data = JSON.parse(rawdata.toString());
+    let projectDir = data.project_config[0].directory;
+    OSProcess.testfunction(projectDir, "minifab netup");
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
