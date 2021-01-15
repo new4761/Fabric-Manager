8d286aa069dfd0be5c896a5c0ed4e9fddc742775
pisichi <win.pisichi@gmail.com>
2021-01-15 09:17:00 +0700

    add args for OSprocess, logging stdout

--- desktopapp/src/module/FileJsonBuilder.ts
+++ desktopapp/src/module/FileJsonBuilder.ts
@@ -4,32 +4,29 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 const editJsonFile = require("edit-json-file");
 const path = require("path");
 export class FileJsonBuilder {
-  // config:object;
-  // project_config:object;
   file: any;
 
   constructor() {
-    this.file = editJsonFile("./tests/test.json");
-    // this.file.set("project_config", []);
-    // this.file.save();
+    this.file = editJsonFile("./tests/net-config.json");
   }
 
   addProject(project: object) {
     let data = yaml.load(fs.readFileSync("./tests/spec.yaml", "utf8"));
     console.log(data);
-    let projects = this.file.get().project_config;
     data = {
       ...project,
       date_modify: +new Date(),
       ...data,
     };
-    projects.push(data);
-    this.file.set("project_config", projects);
+    this.file.set("project_config", data);
     this.file.save();
   }
 
-  updateProject() {}
+  updateConfig(key:string, value:any) {
+    this.file.set(key, value);
+    this.file.save();
+  }
 
-  deleteProject() {}
+  
 }
 export default new FileJsonBuilder();
--- desktopapp/src/module/OSProcess.ts
+++ desktopapp/src/module/OSProcess.ts
@@ -1,28 +1,36 @@
-const  { exec }  = require("child_process");
+const { exec } = require("child_process");
+const { spawn } = require('child_process');
+const path = require("path");
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 class OSProcess {
   constructor() {}
-  testfunction(outputPath: string,command: string) {
-   
-    let netupCMD = "minifab netup -o org0.example.com";
-    console.log(outputPath);
-    console.log(netupCMD);
-    exec(
-      netupCMD,
-      { cwd: outputPath },
-      (error: any, data: any, getter: any) => {
-        if (error) {
-          console.log("error", error.message);
-          return;
-        }
-        if (getter) {
-          console.log("data", data);
-          return;
-        }
-        console.log("data", data);
-      }
-    );
-  }
 
+  run(path: string, args:string[]) {
+
+    const dir = spawn('minifab.cmd',args, {shell: true,cwd: path });
+
+
+    dir.stdout.on('data', (data: any) => {
+      console.log(`${data}`);
+    });
+  
+    dir.stderr.on('data', (data: any) => {
+      console.log(`spawn stderr: ${data}`);
+    });
+  
+    dir.on('error', (code: any) => {
+      console.log(`spawn error: ${code}`);
+    });
+  
+    dir.on('close', (code: any) => {
+      console.log(`spawn child process closed with code ${code}`);
+    });
+  
+    dir.on('exit', (code: any) => {
+      console.log(`spawn child process exited with code ${code}`);
+    });
+
+  }
 }
 export default new OSProcess();
