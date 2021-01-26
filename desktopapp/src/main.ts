02c651f2a157421083daa6ba6b7da7c076c105bb
pisichi <win.pisichi@gmail.com>
2021-01-26 14:09:46 +0700

    add menu

--- desktopapp/src/main.ts
+++ desktopapp/src/main.ts
@@ -73,8 +73,8 @@ import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
-// import './assets/layout/layout.scss';
-// import './assets/layout/flags/flags.css';
+import './assets/layout/layout.scss';
+import './assets/layout/flags/flags.css';
 
 
 Vue.use(ToastService);
