83f61768c6ffd4802d134c54e8767c4c058e442e
new4761 <brid47@outlook.com>
2021-06-01 20:04:37 +0700

    :rocket: Fix installer

--- desktopapp/vue.config.js
+++ desktopapp/vue.config.js
@@ -1,15 +1,41 @@
-const ICONS_DIR = 'build/icons'
-module.exports = {
 
+
+const ICONS_DIR = "build/icons";
+const app_version = "0.1-alpha";
+module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
-      externals: ["fabric-protos","fabric-network","fabric-common","fabric-ca-client"],
+      externals: [
+        "fabric-protos",
+        "fabric-network",
+        "fabric-common",
+        "fabric-ca-client",
+      ],
       builderOptions: {
-        asar: false,
+        //asar: false,
         appId: "com.electron.Fabric-Manager",
-        productName :"Fabric-Manager",
-        win:{icon:ICONS_DIR},
+        productName: "Fabric-Manager",
+        buildVersion:app_version,
+        win: { icon: ICONS_DIR ,target:["nsis"]},
+        // msi:{
+        //   allowElevation: false,
+        //   allowToChangeInstallationDirectory: true,
+        //   oneClick: false,
+        //   perMachine: true,
+         
+        // },
+        nsis: {
+          allowElevation: false,
+          allowToChangeInstallationDirectory: true,
+          oneClick: false,
+          perMachine: true,
+          //include:"build/installer.nsh",
+          uninstallDisplayName:"unins000"
+          //createDesktopShortcut : false,
+          //createStartMenuShortcut : false,
+          //menuCategory :true
+        },
         extraResources: [
           { from: "extraResources/example_code", to: "example_code" },
           { from: "extraResources/log", to: "extraResources/log" },
